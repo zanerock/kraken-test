10f41962d99df2c955b7785f793facd41df459af
Zane Rockenbaugh <zane@liquid-labs.com>
2024-07-31 19:44:44 -0500

    flesh out cli tests

--- src/cli/test/site-outage-reporter-cli.test.mjs
+++ src/cli/test/site-outage-reporter-cli.test.mjs
@@ -16,4 +16,23 @@ describe('siteOutageReporterCLI', () => {
     test('default cutoffTime', () => expect(testOptions.cutoffTime).toEqual(new Date('2022-01-01T00:00:00.000Z')))
     test('default siteID', () => expect(testOptions.siteID).toBe('norwich-pear-tree'))
   })
+
+  test.each([
+    ['2022-01-02 12:30 Z', new Date('2022-01-02T12:30:00.000Z')],
+    ['01-02-2022 12:30 Z', new Date('2022-01-02T12:30:00.000Z')],
+    ['2 Jan 2022 12:30:30.123 Z', new Date('2022-01-02T12:30:30.123Z')],
+  ])('accepts and parses cutoff time string %s', async (timeString, expectedTime) => {
+    let testOptions
+    siteOutageReporter.mockImplementation(async (options) => { testOptions = options })
+    await siteOutageReporterCLI({ argv : ['--cutoff-time', timeString] })
+    expect(testOptions.cutoffTime).toEqual(expectedTime)
+  })
+
+  test('sets siteID from argv', async () => {
+    const siteID = 'abc'
+    let testOptions
+    siteOutageReporter.mockImplementation(async (options) => { testOptions = options })
+    await siteOutageReporterCLI({ argv : ['--site-id', siteID] })
+    expect(testOptions.siteID).toEqual(siteID)
+  })
 })
